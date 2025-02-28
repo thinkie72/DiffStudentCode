8eedac57f34756512c42b768775f93c5f9c078d0
thinkie72 <tyler.hinkie@menloschool.org>
2025-02-27 22:06:53 -0800

    Work Log Entries 1-3

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -3,7 +3,7 @@
  * A tool for finding the longest shared substring between two documents.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Tyler Hinkie
  */
 public class PlagiarismChecker {
 
@@ -17,7 +17,19 @@ public class PlagiarismChecker {
     public static int longestSharedSubstring(String doc1, String doc2) {
 
         // TODO Complete this function to return the length of the longest shared substring.
+        int[][] tabulation = new int[doc1.length() + 1][doc2.length() + 1];
+        int test;
+        for (int i = 1; i < tabulation.length; i++) {
+            for (int j = 1; j < tabulation[0].length; j++) {
+                if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+                    test = tabulation[i - 1][j];
+                    if (test <= i && test <= j) tabulation[i][j] = test + 1;
+                    else tabulation[i][j] = test;
+                }
+                else tabulation[i][j] = Math.max(tabulation[i - 1][j], tabulation[i][j - 1]);
+            }
+        }
 
-        return 0;
+        return tabulation[tabulation.length - 1][tabulation[0].length - 1];
     }
 }
